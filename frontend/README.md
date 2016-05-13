f75314fb2f165a8d8b933b639384bf64c1fed413
mateuszdargacz <mateuszdargacz@gmail.com>
2016-05-13 11:04:44 +0200

    Frontend initial commit

---
+++ frontend/README.md
@@ -0,0 +1,54 @@
+
+### Quick start Frontend
+**Make sure you have Node version >= 4.0 and NPM >= 3**
+> Clone/Download the repo then edit `app.ts` inside [`/src/app/app.ts`](/src/app/app.ts)
+
+```bash
+
+# add required global libraries
+npm install typings webpack-dev-server rimraf webpack -g
+
+# install the repo with npm
+npm install
+
+# start the server
+npm start
+
+# use Hot Module Replacement
+npm run server:dev:hmr
+
+```
+go to [http://0.0.0.0:3000](http://0.0.0.0:3000) or [http://localhost:3000](http://localhost:3000) in your browser
+
+# Getting Started
+## Dependencies
+What you need to run this app:
+* `node` and `npm` (`brew install node`)
+* Ensure you're running the latest versions Node `v4.x.x`+ (or `v5.x.x`) and NPM `3.x.x`+
+
+Once you have those, you should install these globals with `npm install --global`:
+* `webpack` (`npm install --global webpack`)
+* `webpack-dev-server` (`npm install --global webpack-dev-server`)
+* `karma` (`npm install --global karma-cli`)
+* `protractor` (`npm install --global protractor`)
+* `typings` (`npm install --global typings`)
+* `typescript` (`npm install --global typescript`)
+
+
+# Quickstart Backend
+## Dependencies
+
+* `virtualenv env --no-site-packages`
+* `env/bin/activate`
+* `pip install requrements.txt`
+
+## Setup
+
+* `python manage.py migrate`
+* `python manage.py createsuperuser`
+* `python manage.py runserver`
+
+Server is ready to go at 0.0.0.0:8000
+
+
+
---
+++ frontend/src/app/app.component.js
@@ -0,0 +1,54 @@
+/*
+ * Angular 2 decorators and services
+ */
+var core_1 = require('@angular/core');
+var router_deprecated_1 = require('@angular/router-deprecated');
+var app_service_1 = require('./app.service');
+var home_1 = require('./home');
+var router_active_1 = require('./router-active');
+/*
+ * App Component
+ * Top Level Component
+ */
+var App = (function () {
+    function App(appState) {
+        this.appState = appState;
+        this.angularclassLogo = 'assets/img/angularclass-avatar.png';
+        this.loading = false;
+        this.name = 'Angular 2 Webpack Starter';
+        this.url = 'https://twitter.com/AngularClass';
+    }
+    App.prototype.ngOnInit = function () {
+        console.log('Initial App State', this.appState.state);
+    };
+    App = __decorate([
+        core_1.Component({
+            selector: 'app',
+            pipes: [],
+            providers: [],
+            directives: [router_active_1.RouterActive],
+            encapsulation: core_1.ViewEncapsulation.None,
+            styles: [
+                require('./app.css')
+            ],
+            template: "\n    <span router-active>\n      <button [routerLink]=\" ['Index'] \">\n        Index\n      </button>\n    </span>\n    <span router-active>\n      <button [routerLink]=\" ['Home'] \">\n        Home\n      </button>\n    </span>\n    <span router-active>\n      <button [routerLink]=\" ['About'] \">\n        About\n      </button>\n    </span>\n\n    <main>\n      <router-outlet></router-outlet>\n    </main>\n\n    <pre class=\"app-state\">this.appState.state = {{ appState.state | json }}</pre>\n  "
+        }),
+        router_deprecated_1.RouteConfig([
+            { path: '/', name: 'Index', component: home_1.Home, useAsDefault: true },
+            { path: '/home', name: 'Home', component: home_1.Home },
+            // Async load a component using Webpack's require with es6-promise-loader and webpack `require`
+            { path: '/about', name: 'About', loader: function () { return require('es6-promise!./about')('About'); } }
+        ]), 
+        __metadata('design:paramtypes', [app_service_1.AppState])
+    ], App);
+    return App;
+})();
+exports.App = App;
+/*
+ * Please review the https://github.com/AngularClass/angular2-examples/ repo for
+ * more angular app examples that you may copy/paste
+ * (The examples may not be updated as quickly. Please open an issue on github for us to update it)
+ * For help or questions please contact us at @AngularClass on twitter
+ * or our chat on Slack at https://AngularClass.com/slack-join
+ */
+//# sourceMappingURL=app.component.js.map
